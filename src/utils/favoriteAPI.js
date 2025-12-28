0cb141e371061aef7e27391fa0d5e7a04046f275
13kritan <13kritan@gmail.com>
2025-12-28 06:44:11 +0545

    axios

--- src/utils/favoriteAPI.js
+++ src/utils/favoriteAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/favorites";
+const API = "https://cookle.onrender.com/api/favorites";
 
 const authHeader = () => ({
   headers: {
