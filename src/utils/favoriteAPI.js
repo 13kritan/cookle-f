4d28a8143cc8c282241c646782278f0491837948
13kritan <13kritan@gmail.com>
2025-12-30 02:15:40 +0545

    Update favoriteAPI.js

--- src/utils/favoriteAPI.js
+++ src/utils/favoriteAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "cookle-production.up.railway.app";
+const API = "cookle-production.up.railway.app/api/favorites";
 
 const authHeader = () => ({
   headers: {
